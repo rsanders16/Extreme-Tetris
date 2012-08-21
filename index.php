f57fbdf4a6d89b7b24e1b379be2ff6e07f53d889
Ryan <rks@iastate.edu>
2012-08-20 23:39:55 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Ryan K Sanders - Tetris Demo</title>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33983193-1']);
+  _gaq.push(['_setDomainName', 'ryan-sanders.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+
+<body style=" margin:0px; background-image:url(images/background.png); background-repeat:no-repeat; background-color:#080000; background-position:top">
+<div id="applet" style="padding-top:165px; padding-left:53%;">
+<applet code="AppletStart.class" name=Tetris archive=tetrisSmall.jar
+	width=300 height=500>
+	<param name="bgcolor" value="#080000">
+	<param name="fontcolor" value="#080000">
+	Your browser is not Java enabled.
+</applet>
+</div>
+</body>
+</html>
